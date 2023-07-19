01c44b819551b441e6f64c7b16b52451d5c3115a
DuyNguyen <119731750+NguyenVanDuy2003@users.noreply.github.com>
2023-07-19 16:58:31 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import { hostname } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //   }),
-  // );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   app.enableCors();
   await app.listen(process.env.PORT, '0.0.0.0');
 }
